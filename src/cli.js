d3cd877b956c76f62ee034151dd3b02b2f7c0d55
matheus6p <mhpaes97@gmail.com>
2022-04-27 18:03:16 -0300

    resolvido [ obejct Object ]

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ const caminho = process.argv;
 async function processaTexto(caminho) {
   const resultado = await pegaArquivo(caminho[2]);
   if (caminho[3] === "validar") {
-    console.log(chalk.yellow("Links Validados"), validaURLs(resultado));
+    console.log(chalk.yellow("Links Validados: "), await validaURLs(resultado));
   } else {
     console.log(chalk.yellow("Lista de links: "), resultado);
   }
--- src/validacao-http.js
+++ src/validacao-http.js
@@ -1,12 +1,32 @@
-function geraArrayDeURLs(arrayLinks) {
-  //loop para cada {chave: valor}
-  //objeto -> [valor]
-  //Object.value(objeto) ===== esse metodo recebe um objeto e retorna somente o valor;
-  return arrayLinks.map((objetoLink) => Object.values(objetoLink).join());
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+async function checaStatus(arrayURLs) {
+  const arrayStatus = await Promise.all(
+    arrayURLs.map((arrayURL) => {
+      return Promise.all(
+        arrayURL.map(async (url) => {
+          const res = await fetch(url);
+          return res.status;
+        })
+      );
+    })
+  );
+  return arrayStatus;
 }
 
-function validaURLs(arrayLinks) {
-  return geraArrayDeURLs(arrayLinks);
+function geraArrayDeURLs(arrayLinks) {
+  return arrayLinks.map((links) =>
+    links.map((objetoLink) => {
+      return Object.values(objetoLink).join();
+    })
+  );
+}
+
+async function validaURLs(arrayLinks) {
+  const links = geraArrayDeURLs(arrayLinks);
+  const statusLinks = await checaStatus(links);
+  return statusLinks;
 }
 
 export default validaURLs;
